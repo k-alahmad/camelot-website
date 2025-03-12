fc859c9ad4b771d7e7693ea5ca2dc1d7037cf15c
Kanaan Alahmad <46397358+k-alahmad@users.noreply.github.com>
2025-03-12 22:19:20 +0400

    Testing Home page data

--- src/data/homeData.js
+++ src/data/homeData.js
@@ -1,24 +1,178 @@
+// export const homeData = {
+//   Info: {
+//     titleEn: "Allaham Trading co",
+//     titleAr: "شركة اللحام للتجارة",
+//     DescriptionEn:
+//       "we are passionate about selecting the beautiful decors for your functional spaces hat reflect your unique styles and needs. We are committed to providing a high-quality materials and finishes that will last for years to come.",
+//     DescriptionAr:
+//       "المطبخ هو المركز العصبي للمنزل، وهو اليوم أكثر من أي وقت مضى مكان نعمل فيه ونلعب ونتواصل اجتماعيًا وأيضًا... نطبخ فيه! نقدم لكم مجموعتنا الجديدة من أسطح العمل ذات الحواف المستقيمة.",
+//   },
+//   Profile: {
+//     titleEn: "Our Vision",
+//     titleAr: "اكتشف جميع التصاميم",
+//     DescriptionEn:
+//       "We envision homes that are not only beautiful but also functional and sustainable.That's why we are committed to using sustainable materials and practices whenever possible.",
+//     DescriptionAr:
+//       "قم بجولة في بعض أعمالنا التي تم تصميمها وتصنيعها على يد نخبة من المصممين والحرفيين",
+//   },
+//   Contact: {
+//     titleEn: "We are happy to hear from you",
+//     titleAr: "ونحن سعداء أن نسمع منك",
+//     DescriptionEn: "Contact us and tell us your opinion and suggestions",
+//     DescriptionAr: "تواصل معنا وأخبرنا برأيك واقتراحاتك",
+//   },
+// };
 export const homeData = {
-  Info: {
-    titleEn: "Allaham Trading co",
-    titleAr: "شركة اللحام للتجارة",
-    DescriptionEn:
-      "we are passionate about selecting the beautiful decors for your functional spaces hat reflect your unique styles and needs. We are committed to providing a high-quality materials and finishes that will last for years to come.",
-    DescriptionAr:
-      "المطبخ هو المركز العصبي للمنزل، وهو اليوم أكثر من أي وقت مضى مكان نعمل فيه ونلعب ونتواصل اجتماعيًا وأيضًا... نطبخ فيه! نقدم لكم مجموعتنا الجديدة من أسطح العمل ذات الحواف المستقيمة.",
+  HeroSection: {
+    Header: {
+      titleEn: "English Language School Camelot",
+      titleAr: "مدرسة كاميلوت لتعليم اللغة الإنجليزية",
+    },
+    SubHeader: {
+      titleEn: "Your fast way to results",
+      titleAr: "طريقك السريع نحو النتائج",
+    },
+    CallToAction: {
+      Title: {
+        titleEn: "Sign up for a trial lesson and get a personalised training plan for free",
+        titleAr: "سجل للحصول على درس تجريبي واحصل على خطة تدريب مخصصة مجانًا",
+      },
+      Button: {
+        titleEn: "Sign up form",
+        titleAr: "نموذج التسجيل",
+      },
+      FormFields: {
+        FullName: {
+          titleEn: "Full Name",
+          titleAr: "الاسم الكامل",
+        },
+        PhoneNumber: {
+          titleEn: "Phone number",
+          titleAr: "رقم الهاتف",
+        },
+        Email: {
+          titleEn: "Email",
+          titleAr: "البريد الإلكتروني",
+        },
+      },
+    },
+    HeroImageOrVideo: {
+      titleEn: "Must be discussed",
+      titleAr: "يجب مناقشته",
+    },
   },
-  Profile: {
-    titleEn: "Our Vision",
-    titleAr: "اكتشف جميع التصاميم",
-    DescriptionEn:
-      "We envision homes that are not only beautiful but also functional and sustainable.That's why we are committed to using sustainable materials and practices whenever possible.",
-    DescriptionAr:
-      "قم بجولة في بعض أعمالنا التي تم تصميمها وتصنيعها على يد نخبة من المصممين والحرفيين",
+  WhatWillYouGet: {
+    Title: {
+      titleEn: "What will you get?",
+      titleAr: "ماذا ستحصل؟",
+    },
+    IndividualPlan: {
+      Title: {
+        titleEn: "Individual plan",
+        titleAr: "خطة فردية",
+      },
+      Description: {
+        titleEn:
+          "We’re not a one-size-fits-all school—we adapt our approach to fit your unique learning style. Instead of relying on a single method, we carefully select the one that works best for you. Whether you thrive in a dynamic group setting or prefer one-on-one lessons, need the flexibility of online learning or the engagement of in-person classes, we tailor the experience to suit your needs. Our goal is to create the perfect learning environment where you can progress confidently and effectively.",
+        titleAr:
+          "نحن لسنا مدرسة ذات مقاس واحد يناسب الجميع - بل نكيف منهجنا ليناسب أسلوب التعلم الفريد الخاص بك. بدلاً من الاعتماد على طريقة واحدة، نختار بعناية الطريقة التي تناسبك بشكل أفضل. سواء كنت تزدهر في بيئة جماعية ديناميكية أو تفضل الدروس الفردية، أو تحتاج إلى مرونة التعلم عبر الإنترنت أو المشاركة في الفصول الدراسية الشخصية، فإننا نصمم التجربة لتناسب احتياجاتك. هدفنا هو خلق بيئة تعليمية مثالية حيث يمكنك التقدم بثقة وفعالية.",
+      },
+    },
+    PersonalCurator: {
+      Title: {
+        titleEn: "Personal curator",
+        titleAr: "مرشد شخصي",
+      },
+      Description: {
+        titleEn:
+          "You're never alone on your learning journey! In addition to your teacher, a dedicated member of our team will always be there to support you. Whether you have questions, face challenges, or need a little extra motivation, we’re here to help every step of the way. From answering doubts to keeping you inspired and on track, our team ensures that you stay confident, engaged, and committed to achieving your language goals.",
+        titleAr:
+          "أنت لست وحدك أبدًا في رحلة التعلم الخاصة بك! بالإضافة إلى معلمك، سيكون هناك دائمًا عضو مخصص من فريقنا لدعمك. سواء كانت لديك أسئلة أو واجهت تحديات أو كنت بحاجة إلى القليل من التحفيز الإضافي، فنحن هنا للمساعدة في كل خطوة على الطريق. من الإجابة على الشكوك إلى إبقائك ملهمًا وعلى المسار الصحيح، يضمن فريقنا أن تظل واثقًا ومشاركًا وملتزمًا بتحقيق أهدافك اللغوية.",
+      },
+    },
+    NotLessonsButAHobby: {
+      Title: {
+        titleEn: "Not lessons, but a hobby",
+        titleAr: "ليس دروسًا، بل هواية",
+      },
+      Description: {
+        titleEn:
+          "Engage in lively and thought-provoking conversations with your teacher on a wide range of exciting topics—anything from pop culture and your favorite TV series to unexpected and unconventional subjects like sugar daddies. Say goodbye to dull, scripted dialogues about Mary and Jack from outdated textbooks! Instead, you'll dive into real, meaningful discussions that keep you engaged, help you express yourself naturally, and make learning English an enjoyable and immersive experience.",
+        titleAr:
+          "انخرط في محادثات حيوية ومثيرة للتفكير مع معلمك حول مجموعة واسعة من المواضيع المثيرة - أي شيء من الثقافة الشعبية وسلسلتك التلفزيونية المفضلة إلى مواضيع غير متوقعة وغير تقليدية. قل وداعًا للحوارات المملة والمكتوبة عن ماري وجاك من الكتب المدرسية القديمة! بدلاً من ذلك، ستغوص في مناقشات حقيقية وذات مغزى تبقيك منخرطًا، وتساعدك على التعبير عن نفسك بشكل طبيعي، وتجعل تعلم اللغة الإنجليزية تجربة ممتعة وغامرة.",
+      },
+    },
+    Image: {
+      titleEn: "………….",
+      titleAr: "………….",
+    },
   },
-  Contact: {
-    titleEn: "We are happy to hear from you",
-    titleAr: "ونحن سعداء أن نسمع منك",
-    DescriptionEn: "Contact us and tell us your opinion and suggestions",
-    DescriptionAr: "تواصل معنا وأخبرنا برأيك واقتراحاتك",
+  SignUpForTrial: {
+    Title: {
+      titleEn: "Sign up for a trial lesson if:",
+      titleAr: "سجل للحصول على درس تجريبي إذا:",
+    },
+    IELTS: {
+      Description: {
+        titleEn:
+          "You are taking IELTS soon. Prepare with an experienced IELTS examiner who knows the insider strategies for achieving high scores, offering tailored feedback and proven techniques to help you succeed.",
+        titleAr:
+          "أنت مقبل على اختبار IELTS قريبًا. استعد مع ممتحن IELTS ذي خبرة يعرف الاستراتيجيات الداخلية لتحقيق درجات عالية، ويقدم ملاحظات مخصصة وتقنيات مثبتة لمساعدتك على النجاح.",
+      },
+    },
+    ForeignUniversity: {
+      Description: {
+        titleEn:
+          "Do you dream of entering a foreign university? Pass any entrance exam with confidence, and you’ll be well-prepared to excel in your studies. With the right preparation and skills, you’ll not only succeed in the exam but also feel ready to tackle the challenges ahead in your academic journey.",
+        titleAr:
+          "هل تحلم بالدخول إلى جامعة أجنبية؟ اجتز أي امتحان دخول بثقة، وستكون مستعدًا جيدًا للتفوق في دراستك. بالاستعداد والمهارات المناسبة، لن تنجح في الامتحان فحسب، بل ستشعر أيضًا بالاستعداد لمواجهة التحديات المقبلة في رحلتك الأكاديمية.",
+      },
+    },
+    InternationalCompany: {
+      Description: {
+        titleEn:
+          "You work in an international company. Improve your chance of a promotion and open doors to exciting opportunities, including the chance to work abroad, advance your career and broaden your professional horizons.",
+        titleAr:
+          "أنت تعمل في شركة دولية. حسّن فرصتك في الترقية وافتح الأبواب لفرص مثيرة، بما في ذلك فرصة العمل في الخارج، والتقدم في حياتك المهنية وتوسيع آفاقك المهنية.",
+      },
+    },
+  },
+  CallToActionSection: {
+    Title: {
+      titleEn: "Take a language level test and get feedback from your teacher",
+      titleAr: "قم بإجراء اختبار مستوى اللغة واحصل على تعليقات من معلمك",
+    },
+    Description: {
+      titleEn: "Only 20 minutes online",
+      titleAr: "20 دقيقة فقط عبر الإنترنت",
+    },
+    Button: {
+      titleEn: "Pass to direct to test page",
+      titleAr: "انتقل لتوجيهك إلى صفحة الاختبار",
+    },
+  },
+  HowToStartSpeaking: {
+    Title: {
+      titleEn: "How to start speaking English in just a month?",
+      titleAr: "كيف تبدأ التحدث باللغة الإنجليزية في شهر واحد فقط؟",
+    },
+    Steps: [
+      {
+        titleEn: "Sign up for a free trial lesson",
+        titleAr: "سجل للحصول على درس تجريبي مجاني",
+      },
+      {
+        titleEn: "Get a personalised training plan",
+        titleAr: "احصل على خطة تدريب مخصصة",
+      },
+      {
+        titleEn: "Study at a comfortable pace with the support of a tutor",
+        titleAr: "ادرس بوتيرة مريحة بدعم من مدرس",
+      },
+      {
+        titleEn: "Done. You're great!",
+        titleAr: "تم. أنت رائع!",
+      },
+    ],
   },
 };
