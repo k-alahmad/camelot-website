f9ff524f09706a3b8c310ead99c43ca69224021c
MoazAbdulHalim <moazabdulhalim@gmail.com>
2025-03-23 20:18:14 +0300

    change loader

--- src/components/UI/Loader/index.jsx
+++ src/components/UI/Loader/index.jsx
@@ -1,15 +1,24 @@
+// import React from "react";
+// import "./Loader.css";
+
+// const Loader = () => {
+//   return (
+//     <div className="container">
+//       <span></span>
+//       <span></span>
+//       <span></span>
+//       <span></span>
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
 import React from "react";
 import "./Loader.css";
 
 const Loader = () => {
-  return (
-    <div className="container">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  );
+  return <div className="loader"></div>;
 };
 
 export default Loader;
