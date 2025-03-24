49beef1fd8d06f02ab210b825e93f954051e2120
MoazAbdulHalim <moazabdulhalim@gmail.com>
2025-03-24 03:45:32 +0300

    tes data file

---
+++ src/data/testData.js
@@ -0,0 +1,107 @@
+export const testData = [
+  {
+    id: 1,
+    question: "My friend used to ________ to my house every day after school.",
+    answers: [
+      { id: "A", answer: "come", flag: true },
+      { id: "B", answer: "comes", flag: false },
+      { id: "C", answer: "came", flag: false },
+      { id: "D", answer: "comming", flag: false },
+    ],
+  },
+  {
+    id: 2,
+    question:
+      "I used to play tennis every day, but I ______ anymore because I hurt my back.",
+    answers: [
+      { id: "A", answer: "no play", flag: false },
+      { id: "B", answer: "don't play", flag: true },
+      { id: "C", answer: "not play", flag: false },
+      { id: "D", answer: "not playing", flag: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "I am 16 and my brother is 22, so he’s ______ me.",
+    answers: [
+      { id: "A", answer: "as old as", flag: false },
+      { id: "B", answer: "older than", flag: true },
+      { id: "C", answer: "older", flag: false },
+      { id: "D", answer: "eldest of", flag: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Olga ______ her hand badly on the barbeque last weekend.",
+    answers: [
+      { id: "A", answer: "scratched", flag: false },
+      { id: "B", answer: "broke", flag: false },
+      { id: "C", answer: "hurted", flag: false },
+      { id: "D", answer: "burned", flag: true },
+    ],
+  },
+
+  {
+    id: 5,
+    question: "Tomorrow is a public holiday, so we ____ go to school.",
+    answers: [
+      { id: "A", answer: "don’t have to", flag: true },
+      { id: "B", answer: "have to", flag: false },
+      { id: "C", answer: "can", flag: false },
+      { id: "D", answer: "mustn't", flag: false },
+    ],
+  },
+  {
+    id: 6,
+    question:
+      "She ______ landed by now. I don’t understand why she hasn’t called",
+    answers: [
+      { id: "A", answer: "must be", flag: false },
+      { id: "B", answer: "should", flag: false },
+      { id: "C", answer: "should be", flag: false },
+      { id: "D", answer: "must have", flag: true },
+    ],
+  },
+  {
+    id: 7,
+    question: "I _______ working today and tomorrow this week.",
+    answers: [
+      { id: "A", answer: "will", flag: false },
+      { id: "B", answer: "am", flag: true },
+      { id: "C", answer: "have", flag: false },
+      { id: "D", answer: "will to", flag: false },
+    ],
+  },
+  {
+    id: 8,
+    question: "______ we call a plumber; the taps will continue to leak.",
+    answers: [
+      { id: "A", answer: "if", flag: false },
+      { id: "B", answer: "until", flag: true },
+      { id: "C", answer: "should", flag: false },
+      { id: "D", answer: "could", flag: false },
+    ],
+  },
+  {
+    id: 9,
+    question:
+      "we _______ call Margret and see if she has the results of her blood test.",
+    answers: [
+      { id: "A", answer: "should", flag: true },
+      { id: "B", answer: "shouldn't to", flag: false },
+      { id: "C", answer: "should to", flag: false },
+      { id: "D", answer: "can", flag: false },
+    ],
+  },
+  {
+    id: 10,
+    question:
+      "As soon as I got ____ the taxi, I realised I had left my wallet in the restaurant.",
+    answers: [
+      { id: "A", answer: "into", flag: true },
+      { id: "B", answer: "in", flag: false },
+      { id: "C", answer: "on", flag: false },
+      { id: "D", answer: "onto", flag: false },
+    ],
+  },
+];
