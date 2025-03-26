2b77e6558c2a48ea00d580c2fc5437201119ae90
MoazAbdulHalim <moazabdulhalim@gmail.com>
2025-03-26 18:51:56 +0300

    auto play

--- src/pages/home/components/Reviews.jsx
+++ src/pages/home/components/Reviews.jsx
@@ -23,6 +23,8 @@ const Reviews = () => {
         infinite={true}
         slidesToShow={3}
         slidesToScroll={1}
+        autoplay={true}
+        autoplaySpeed={5000}
         speed={500}
         touchMove={true}
         nextArrow={<SampleNextArrow />}
